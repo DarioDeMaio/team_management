5b60796c78c711fe998e198bfc35dcceb60eb96f
DarioDeMaio <demaio.dario95@gmail.com>
2024-08-22 11:26:54 +0200

    fix(project_route)

--- projects/routes.py
+++ projects/routes.py
@@ -53,6 +53,8 @@ def project_details(project_id):
 
 @projects_bp.route("/edit_project/<project_id>", methods=["GET", "POST"])
 def edit_project(project_id):
+    project = projects.find_one({"_id": ObjectId(project_id)})
+    
     if request.method == "POST":
         updated_members = [ObjectId(member_id) for member_id in request.form.getlist('members')]
         updated_project = {
@@ -66,25 +68,29 @@ def edit_project(project_id):
         for member_id in updated_members:
             user = users.find_one({"_id": member_id})
             if user:
-                if 'Projects' in user and ObjectId(project_id) not in user['Projects']:
+                if 'Projects' not in user:
+                    user['Projects'] = []
+                if ObjectId(project_id) not in user['Projects']:
                     user['Projects'].append(ObjectId(project_id))
-                else:
-                    user['Projects'] = [ObjectId(project_id)]
                 users.update_one({"_id": member_id}, {"$set": {"Projects": user['Projects']}})
         
-        current_project = projects.find_one({"_id": ObjectId(project_id)})
-        current_members = [ObjectId(member_id) for member_id in current_project.get('Members', [])]
+        current_members = set(ObjectId(member_id) for member_id in project.get('Members', []))
+        updated_members_set = set(updated_members)
         
-        for member_id in current_members:
-            if member_id not in updated_members:
-                result = projects.update_one(
-                    {"_id": member_id},
-                    {"$pull": {'Members': ObjectId(project_id)}}
-                )
-                print(f"Removing project {project_id} from user {member_id}. Matched: {result.matched_count}, Modified: {result.modified_count}")
+        for member_id in current_members - updated_members_set:
+            result = users.update_one(
+                {"_id": member_id},
+                {"$pull": {"Projects": ObjectId(project_id)}}
+            )
         
         return redirect(url_for('projects_bp.get_projects'))
     
     user_list = list(users.find())
     project = projects.find_one({"_id": ObjectId(project_id)})
-    return render_template("projects/edit_project.html", user_list=user_list, project=project)
+    return render_template("projects/edit_project.html", project=project, user_list=user_list)
+
+
+@projects_bp.route("/delete_project/<project_id>", methods=["POST"])
+def delete_project(project_id):
+    projects.delete_one({"_id":ObjectId(project_id)})
+    return redirect(url_for('projects_bp.get_projects'))
